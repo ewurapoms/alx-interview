03ec8d1ee67653cb0100c9db76b1c18f65bd1435
ewurapoms <mzpomaa35@gmail.com>
2024-03-07 05:05:43 -0800

    still debugging task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,24 +12,25 @@ def validUTF8(data):
     Returns:
         bool: True if data is a valid UTF-8 encoding, else False.
     """
-    num_bytes_to_follow = 0
+    num_bytes = 0
 
-    for num in data:
-        if num_bytes_to_follow == 0:
-            if num >> 7 == 0b0:
-                num_bytes_to_follow = 0
-            elif num >> 5 == 0b110:
-                num_bytes_to_follow = 1
-            elif num >> 4 == 0b1110:
-                num_bytes_to_follow = 2
-            elif num >> 3 == 0b11110:
-                num_bytes_to_follow = 3
+    for byte in data:
+        byte = byte & 0xFF
+
+        if num_bytes == 0:
+            if byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes = 3
+            elif byte >> 7 == 0b0:
+                continue
             else:
                 return False
         else:
-            if num >> 6 != 0b10:
+            if byte >> 6 != 0b10:
                 return False
+            num_bytes -= 1
 
-            num_bytes_to_follow -= 1
-
-    return num_bytes_to_follow == 0
+    return num_bytes == 0
